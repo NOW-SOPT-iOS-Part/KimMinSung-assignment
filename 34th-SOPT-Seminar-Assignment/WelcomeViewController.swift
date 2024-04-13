34be4a494a3133cbb2a788673f01f0aacdb07803
Minsung Kim <mskim4048@naver.com>
2024-04-13 23:37:05 +0900

    InitialViewController 정의 LoginViewController 정의 WelcomeViewController  정의 SceneDelegate.window?.rootViewController 에 InitialViewController 인스턴스 할당

---
+++ 34th-SOPT-Seminar-Assignment/WelcomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeViewController.swift
+//  34th-SOPT-Seminar-Assignment
+//
+//  Created by 김민성 on 2024/04/13.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
