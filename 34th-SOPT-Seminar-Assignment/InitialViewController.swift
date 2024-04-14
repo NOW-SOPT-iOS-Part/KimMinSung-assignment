c86e095e44348e18f32ff57e3ff0614db4678a4c
Minsung Kim <mskim4048@naver.com>
2024-04-14 16:55:51 +0900

    InitialViewController 기본 UI 세팅

--- 34th-SOPT-Seminar-Assignment/InitialViewController.swift
+++ 34th-SOPT-Seminar-Assignment/InitialViewController.swift
@@ -6,24 +6,51 @@
 //
 
 import UIKit
+import SnapKit
 
 class InitialViewController: UIViewController {
 
+    lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("티빙 로그인", for: UIControl.State.normal)
+        button.backgroundColor = .systemRed
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 3
+        button.addTarget(self, action: #selector(loginButtonDidTapped), for: UIControl.Event.touchUpInside)
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .black
+        self.configureHierarchy()
+        self.setLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func configureHierarchy() {
+        self.view.addSubview(loginButton)
     }
-    */
+    
+    private func setLayout() {
+        self.loginButton.translatesAutoresizingMaskIntoConstraints = false
+        self.loginButton.snp.makeConstraints { btn in
+            btn.leading.equalToSuperview().offset(16)
+            btn.trailing.equalToSuperview().offset(-16)
+            btn.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom).offset(-27)
+            btn.height.equalTo(52)
+        }
+    }
+    
+    
+    
+    @objc private func loginButtonDidTapped() {
+        print(#function)
+        //let loginVC = LoginViewController()
+        //loginVC.modalPresentationStyle = .fullScreen
+        //self.present(loginVC, animated: true)
+    }
 
 }
