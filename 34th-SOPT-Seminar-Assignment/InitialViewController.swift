ae09879543029284241473d1fedac09a0eea618a
Minsung Kim <mskim4048@naver.com>
2024-04-17 10:34:29 +0900

    시작 화면 기본 이미지 설정

--- 34th-SOPT-Seminar-Assignment/InitialViewController.swift
+++ 34th-SOPT-Seminar-Assignment/InitialViewController.swift
@@ -9,7 +9,14 @@ import UIKit
 import SnapKit
 
 class InitialViewController: UIViewController {
-
+    
+    let mainImageView: UIImageView = {
+        let kboImage = UIImage(named: "initialImage.kbo")
+        let imageView = UIImageView(image: kboImage)
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
     lazy var loginButton: UIButton = {
         let button = UIButton()
         button.setTitle("티빙 로그인", for: UIControl.State.normal)
@@ -32,11 +39,23 @@ class InitialViewController: UIViewController {
     
     
     private func configureHierarchy() {
-        self.view.addSubview(loginButton)
+        [self.mainImageView, self.loginButton].forEach { view in
+            self.view.addSubview(view)
+        }
     }
     
     private func setLayout() {
-        self.loginButton.translatesAutoresizingMaskIntoConstraints = false
+        [self.mainImageView, self.loginButton].forEach { view in
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        self.mainImageView.snp.makeConstraints { imgView in
+            imgView.top.equalToSuperview()
+            imgView.leading.equalToSuperview()
+            imgView.trailing.equalToSuperview()
+            imgView.bottom.equalToSuperview()
+        }
+        
         self.loginButton.snp.makeConstraints { btn in
             btn.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leading).offset(20)
             btn.trailing.equalTo(self.view.safeAreaLayoutGuide.snp.trailing).offset(-20)
