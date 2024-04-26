cb286ced8f286685dba06ad19c7fc90c81ceb231
Minsung Kim <mskim4048@naver.com>
2024-04-27 02:14:34 +0900

    기본 틀(tabBarCon, naviCon 등) 세팅

---
+++ 34th-SOPT-Seminar-Assignment/otherViewControllers/UpcomingContentsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UpcomingContentsViewController.swift
+//  34th-SOPT-Seminar-Assignment
+//
+//  Created by 김민성 on 2024/04/26.
+//
+
+import UIKit
+
+class UpcomingContentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemGreen
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
