a36998295a0a6c3e8dea279c22bb41fbf98833c6
Minsung Kim <mskim4048@naver.com>
2024-05-09 12:54:37 +0900

    NetworkingManager 싱글톤 객체로 구현

---
+++ 34th-SOPT-Seminar-Assignment/Networking/NetworkingManager.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkingManager.swift
+//  34th-SOPT-Seminar-Assignment
+//
+//  Created by 김민성 on 2024/05/09.
+//
+
+import Foundation
+
+final class NetworkingManager {
+    
+    static let shared = NetworkingManager()
+    private init() { }
+    
+    
+}
