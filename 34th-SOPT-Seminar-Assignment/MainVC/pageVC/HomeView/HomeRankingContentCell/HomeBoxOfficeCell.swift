aa693208e48cfe3a6a1eb8f52ac8f75ffb88e6d2
Minsung Kim <mskim4048@naver.com>
2024-05-10 21:49:00 +0900

    주석 추가

--- 34th-SOPT-Seminar-Assignment/MainVC/pageVC/HomeView/HomeRankingContentCell/HomeBoxOfficeCell.swift
+++ 34th-SOPT-Seminar-Assignment/MainVC/pageVC/HomeView/HomeRankingContentCell/HomeBoxOfficeCell.swift
@@ -8,6 +8,13 @@
 import UIKit
 import SnapKit
 
+/*
+ 랭킹을 나타내는 Cell을 HomeBoxOfficeCell과 HomeLiveContentCell로 분리함.
+ HomeBoxOfficeCell은 날짜별 박스오피스를 보여주면서 reload할 일이 많은데, 이때, 동적으로 셀의 크기를 지정하다 보니 스크롤이 부자연스럽게 끊기는 현상이 있었음.
+ 그래서 AutoLayout Constraint를 제외한 기본 뼈대를 구상하는 HomeRankingContentCell을 만들고, 정적으로 AutoLayout Constraint를 정해 주는 함수를 자식 클래스에 구현하여 추가함.
+ -> 스크롤이 부자연스럽게 끊기는 현상 해결!
+ 다만, HomeRankingContentCell을 추상화하고 싶어서 프로토콜과 확장으로 구현해도 좋을 것 같았는데, 시간이 없어서 못함(양해 바랍니다...ㅠㅅㅠ)
+ */
 class HomeBoxOfficeCell: HomeRankingContentCell {
     
     override init(frame: CGRect) {
