3512cd8c1181013d24b1f8351a41445bf81b4573
Minsung Kim <mskim4048@naver.com>
2024-06-03 22:45:30 +0900

    [Chore] UIView의 확장 구현(addSubviews 메서드)

---
+++ 34th-SOPT-Seminar-Assignment/Global/Extensions/UIViewExtensions.swift
@@ -0,0 +1,16 @@
+//
+//  UIViewExtensions.swift
+//  34th-SOPT-Seminar-Assignment
+//
+//  Created by 김민성 on 2024/06/03.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addSubViews(views: UIView...) {
+        views.forEach { self.addSubview($0) }
+    }
+    
+}
